f18401be54638d1673c28f568af97d4d7ea30d49
Josh <144963859+JoshuaBarrantesMena@users.noreply.github.com>
2024-03-27 21:22:48 -0600

    project creation
    project rising and linked

---
+++ src/main/java/com/josh/numberboard/main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.josh.numberboard;
+
+/**
+ *
+ * @author Josh
+ */
+public class main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
