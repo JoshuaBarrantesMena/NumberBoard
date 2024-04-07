78333021f8e4e1a360853809e94789b2c1f8a714
Jhashua10 <jhashua10@gmail.com>
2024-04-06 21:14:12 -0600

    todo lo nuevo

---
+++ src/main/java/com/josh/numberboard/Clients.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.josh.numberboard;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Clients {
+    private String name;
+    private int ID;
+    private int winNumber;
+    private int phoneNumber;
+
+    public Clients(String name, int ID, int winNumber, int phoneNumber) {
+        this.name = name;
+        this.ID = ID;
+        this.winNumber = winNumber;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Clients() {}
+
+    public String getName() {
+        return name;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public int getWinNumber() {
+        return winNumber;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public void setWinNumber(int winNumber) {
+        this.winNumber = winNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    
+   //b hjahshashsau
+    
+    
+    
+}
